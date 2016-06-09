c08d27464773e3eb07f2deadc84d73f2a0130507
nickolay Gotsliyk <trolizmaslom@gmail.com>
2016-06-09 11:17:24 +0300

    tyn

--- src/js/develop/develop_5.js
+++ src/js/develop/develop_5.js
@@ -1,8 +1,21 @@
-
-
+function butterMenu() {
+    $('.butter').click(function () {
+        $(this).toggleClass('active');
+        if($(this).hasClass('active')){
+            $('.menu.top ul').stop().slideDown();
+        }else{
+            $('.menu.top ul').stop().slideUp();
+        }
+        return false;
+    });
+}
+function checkAdaptive() {
+    if($('.butter').css('display') == 'block' ){return true;}
+    return false;
+}
 
 $(document).ready(function(){
-
+    butterMenu();
 });
 
 $(window).load(function(){
@@ -10,5 +23,8 @@ $(window).load(function(){
 });
 
 $(window).resize(function(){
-
+    if (checkAdaptive()){
+        $('.menu.top ul').removeAttr('style');
+        $('.butter').removeAttr('style');
+    }
 });
