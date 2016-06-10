8101cb02655ab47ef91d8f2a7caea926b43819a2
radiks <radion91@gmail.com>
2016-06-10 14:58:20 +0300

    123123123123

--- src/js/validate_script.js
+++ src/js/validate_script.js
@@ -268,6 +268,7 @@ $(document).ready(function(){
    validate('#call-popup .contact-form', {submitFunction:validationCall});
    validate('.tel-form', {submitFunction:validationCall});
    validate('.tel-form2', {submitFunction:validationCall});
+   validate('.portal-form-form', {submitFunction:validationCall});
    Maskedinput();
    fancyboxForm();
 
