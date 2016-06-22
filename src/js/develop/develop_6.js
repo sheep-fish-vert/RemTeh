bae2f4dd71931a193e0c45a24845de3da0ba3adc
Nazar <kryvyynazariy@gmail.com>
2016-06-22 15:12:25 +0300

    telephone-fix, form-fix, backgroud-fix

--- src/js/develop/develop_6.js
+++ src/js/develop/develop_6.js
@@ -1,8 +1,21 @@
+function headerButer(menuMobile,toggleMenu){
+ 
+    $(document).on('click touchstart',function (event){
+        if($(window).width()<1024-$.scrollbarWidth()){
+            var div = toggleMenu;
+            if (!div.is(event.target) && div.has(event.target).length === 0 && !menuMobile.is(event.target) && menuMobile.has(event.target).length === 0)
+                {
+                    toggleMenu.slideUp();
+                    menuMobile.removeClass('active');
+                }
+        }
+    });
+}
 
 
 
 $(document).ready(function(){
-
+	headerButer($(".butter"),$( ".mbox>ul"));
 });
 
 $(window).load(function(){
